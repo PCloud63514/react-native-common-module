5c084fde82545ae907b8ee7490f832ce93ac4521
PCloud <pcloud63514@gmail.com>
2021-08-05 10:05:22 +0900

    절대경로 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,37 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+          ".png",
+          ".jpg"
+        ],
+        alias: {
+          '@': './src',
+          '@components': './src/ui/components',
+          '@containers': './src/containers',
+          '@images': './assets/images',
+          '@screen': './src/ui/screen',
+          // '@modules': './src/modules',
+          // '@scenes': './src/scenes',
+          '@domain': './src/domain',
+          '@exception': './src/core/exception',
+          '@manager': './src/core/manager',
+          '@utils': './src/core/utils',
+        },
+      },
+    ],
+  ],
 };
